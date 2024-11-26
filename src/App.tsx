a79f8583685df83be48f42f5afa11169bee53f2f
aj.abhijeet21 <aj.abhijeet21@hotmail.com>
2024-11-26 14:13:46 +0530

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
+import Hero from "./components/Hero";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <h1>Hello world!</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="text-red-600 "
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
   );
-}
+};
 
 export default App;
